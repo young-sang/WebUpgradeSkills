7b8842f7bb213e66c2b2782cde8ee51b55d62f49
young-sang <68852430+young-sang@users.noreply.github.com>
2025-01-10 12:09:02 +0900

    옵션 오류 수정

--- VanillaJS+Node.js+리팩토링/public/js/component/posts.js
+++ VanillaJS+Node.js+리팩토링/public/js/component/posts.js
@@ -38,22 +38,8 @@ export const renderPosts = async (container, posts) => {
                 renderPost(container, posts[num]);
             });
         });
-                
-        const scrollableContainer = document.querySelector('.scrollable-container');
-        console.log(scrollableContainer);
-
-        // 스크롤 활성화/비활성화 조건 제어
-        function toggleScroll(enable) {
-            if (enable) {
-                scrollableContainer.style.overflowY = 'auto';
-            } else {
-                scrollableContainer.style.overflowY = 'hidden';
-            }
-        }
 
 
-        // 5초 후 스크롤 비활성화 예제
-        setTimeout(() => toggleScroll(false), 5000);
     } catch (error) {
         console.error("2", error);
     }
