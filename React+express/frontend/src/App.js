b5265cc5276c95779943c857daa8e6d9800af8a4
young-sang <68852430+young-sang@users.noreply.github.com>
2025-01-31 13:53:22 +0900

    1

--- React+express/frontend/src/App.js
+++ React+express/frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import Footer from './component/Footer.js';
 import Header from './component/Header.js';
 import Main from './component/Main/Main.js';
@@ -6,11 +6,11 @@ import Main from './component/Main/Main.js';
 
 function App() {
   return (
-    <Router basename="/WebUpgradeSkills">
+    <BrowserRouter basename={"https://young-sang.github.io/WebUpgradeSkills/"}>
       <Header />
       <Main />
       <Footer />
-    </Router>
+    </BrowserRouter>
   );
 }
 
